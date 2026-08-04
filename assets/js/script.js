d3fd34abe26b727cf6b4529827740b4deaef14df
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-04 14:51:48 +0530

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,13 @@ window.addEventListener("scroll",()=>{
 
     }
 
-    
+    const winScroll=document.documentElement.scrollTop;
+
+    const height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+    const scrolled=(winScroll/height)*100;
+
+    document.getElementById("progress-bar").style.width=scrolled+"%";
+
+});
+
